f568950bb65bd53f36a7f5f43e4aa847f0346499
Timothy <timothysuldayan1@gmail.com>
2025-01-17 21:20:54 -0800

    configured a basic kafka producer

---
+++ data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/producer/KafkaProducer.java
@@ -0,0 +1,19 @@
+package com.example.data_ingestion_service.services.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        log.info("Sending message to status topic: {}", msg);
+        kafkaTemplate.send("status" ,msg);
+    }
+}
