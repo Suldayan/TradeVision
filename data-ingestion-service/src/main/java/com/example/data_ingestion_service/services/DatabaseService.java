7117396ea2edbb43c7d7ae0af6213bea11df93ee
Timothy <timothysuldayan1@gmail.com>
2025-01-22 21:13:16 -0800

    changed market repo to jpa from crud

--- data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/DatabaseService.java
+++ data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/DatabaseService.java
@@ -1,9 +1,10 @@
 package com.example.data_ingestion_service.services;
 
+import com.example.data_ingestion_service.models.RawMarketModel;
 import jakarta.annotation.Nonnull;
 
 import java.util.Set;
 
 public interface DatabaseService {
-    <S> void saveToDatabase(@Nonnull Set<S> entities);
+    void saveToDatabase(@Nonnull Set<RawMarketModel> marketModels);
 }
--- data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/impl/DatabaseServiceImpl.java
+++ data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/impl/DatabaseServiceImpl.java
@@ -1,41 +1,34 @@
 package com.example.data_ingestion_service.services.impl;
 
+import com.example.data_ingestion_service.models.RawMarketModel;
+import com.example.data_ingestion_service.repository.RawMarketModelRepository;
 import com.example.data_ingestion_service.services.DatabaseService;
-import com.example.data_ingestion_service.services.mapper.RepositoryMapper;
 import com.example.data_ingestion_service.services.producer.KafkaProducer;
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Set;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class DatabaseServiceImpl implements DatabaseService {
-    private final RepositoryMapper repositoryMapper;
+    private final RawMarketModelRepository marketModelRepository;
     private final KafkaProducer kafkaProducer;
 
     @Transactional
     @Override
-    public <S> void saveToDatabase(@Nonnull Set<S> entities) {
-        if (entities.isEmpty()) {
+    public void saveToDatabase(@Nonnull Set<RawMarketModel> marketModels) {
+        if (marketModels.isEmpty()) {
             log.warn("The list of entities for saving has been passed but is empty");
         }
-        Class<?> entityClass = entities.iterator().next().getClass();
-        CrudRepository<S, ?> repository = repositoryMapper.getRepository(entityClass);
-        if (repository != null) {
-            repository.saveAll(entities);
-            log.debug("Saving entities of type: {} with set size: {}", entityClass, entities.size());
-
-            // Send completion status to data processing microservice to initialize the processing flow
-            kafkaProducer.sendMessage(String.format("Status: Completed at %s", LocalTime.now()));
-        } else {
-            log.warn("The class of the given entity does not exist within the repository mapping");
-        }
+        marketModelRepository.save(marketModels);
+        // Send completion status to data processing microservice to initialize the processing flow
+        kafkaProducer.sendMessage(String.format("Status: Completed at %s", LocalTime.now()));
     }
 }
