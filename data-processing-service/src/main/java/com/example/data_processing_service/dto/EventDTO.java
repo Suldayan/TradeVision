b301fc4e58c1ef434dbeef55ad13eb1a00fb1d6b
Timothy <timothysuldayan1@gmail.com>
2025-01-23 19:52:46 -0800

    added an event dto that will be the object to be passed through messages

---
+++ data-processing-service/src/main/java/com/example/data_processing_service/dto/EventDTO.java
@@ -0,0 +1,11 @@
+package com.example.data_processing_service.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EventDTO {
+    private String status;
+    private Long timestamp;
+}
