cd71868f986e073af27c2e2d65185f557d2fec35
Timothy <timothysuldayan1@gmail.com>
2025-01-23 20:21:42 -0800

    configured basic save function

--- data-processing-service/src/main/java/com/example/data_processing_service/services/impl/DataPersistenceServiceImpl.java
+++ data-processing-service/src/main/java/com/example/data_processing_service/services/impl/DataPersistenceServiceImpl.java
@@ -5,8 +5,12 @@ import com.example.data_processing_service.repository.processed.MarketModelRepos
 import com.example.data_processing_service.services.DataPersistenceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalTime;
 import java.util.Set;
 
 @Service
@@ -15,8 +19,16 @@ import java.util.Set;
 public class DataPersistenceServiceImpl implements DataPersistenceService {
     private final MarketModelRepository marketModelRepository;
 
+    @Retryable(
+            backoff = @Backoff(delay = 1000)
+    )
+    @Transactional
     @Override
     public void saveToDatabase(Set<MarketModel> marketModels) {
-
+        try {
+            marketModelRepository.saveAll(marketModels);
+        } catch (Exception ex) {
+            log.error("Failed to save to database at: {}", LocalTime.now());
+        }
     }
 }
