3cace7ee2812eb38abb87f559d2ed97342e1a959
Timothy <timothysuldayan1@gmail.com>
2025-01-25 21:55:16 -0800

    added enable kafka

--- data-ingestion-service/src/main/java/com/example/data_ingestion_service/DataIngestionServiceApplication.java
+++ data-ingestion-service/src/main/java/com/example/data_ingestion_service/DataIngestionServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.data_ingestion_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
+@EnableKafka
 public class DataIngestionServiceApplication {
 
 	public static void main(String[] args) {
