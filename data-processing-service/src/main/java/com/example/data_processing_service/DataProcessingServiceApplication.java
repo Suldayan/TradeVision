bf44833947618e3e48dd14195ec952e72eb57f42
Timothy <timothysuldayan1@gmail.com>
2025-01-25 21:52:51 -0800

    added enable kafka annotation

--- data-processing-service/src/main/java/com/example/data_processing_service/DataProcessingServiceApplication.java
+++ data-processing-service/src/main/java/com/example/data_processing_service/DataProcessingServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.data_processing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class DataProcessingServiceApplication {
 
 	public static void main(String[] args) {
