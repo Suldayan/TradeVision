c9c9dbb1bcf23f5f1ab8b896a671fa552d227ae4
Timothy <timothysuldayan1@gmail.com>
2025-02-03 21:50:53 -0800

    added no args and args constructor

--- data-processing-service/src/main/java/com/example/data_processing_service/dto/EventDTO.java
+++ data-processing-service/src/main/java/com/example/data_processing_service/dto/EventDTO.java
@@ -4,10 +4,14 @@ import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class EventDTO {
     @Nonnull
     @NotBlank(message = "Status can't be blank")
