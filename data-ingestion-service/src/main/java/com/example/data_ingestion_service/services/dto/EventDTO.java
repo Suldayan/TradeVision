e38d87ceb75901a3285870ab9df154076d7641b3
Timothy <timothysuldayan1@gmail.com>
2025-02-03 20:33:16 -0800

    configured json message creation for handling event dto for kafka

--- data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/dto/EventDTO.java
+++ data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/dto/EventDTO.java
@@ -3,12 +3,14 @@ package com.example.data_ingestion_service.services.dto;
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @Builder
 public class EventDTO {
     @Nonnull
--- data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/producer/KafkaProducer.java
+++ data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/producer/KafkaProducer.java
@@ -7,13 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.type.SerializationException;
 import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.stereotype.Service;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.CompletableFuture;
 
-@Service
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class KafkaProducer {
@@ -24,7 +27,8 @@ public class KafkaProducer {
     public void sendMessage(@Nonnull EventDTO event) {
         try {
             log.info("Sending message to topic {}: {}", TOPIC, event);
-            CompletableFuture<SendResult<String, EventDTO>> future = kafkaTemplate.send(TOPIC, event);
+            Message<EventDTO> eventDTOMessage = buildJsonMessage(event);
+            CompletableFuture<SendResult<String, EventDTO>> future = kafkaTemplate.send(eventDTOMessage);
             future.thenAccept(result -> log.info("Sent message: {} with offset: {}", event, result.getRecordMetadata())).exceptionallyAsync(ex -> {
                 log.error("Unable to send message at: {}, {}", LocalDateTime.now(), ex.getMessage());
                 return null;
@@ -34,5 +38,12 @@ public class KafkaProducer {
         } catch (KafkaException ex) {
             log.error("Failed to send event: {}", event, ex);
         }
+    }
+
+    private Message<EventDTO> buildJsonMessage(@Nonnull EventDTO event) {
+        return MessageBuilder
+                .withPayload(event)
+                .setHeader(KafkaHeaders.TOPIC, TOPIC)
+                .build();
     }
 }
