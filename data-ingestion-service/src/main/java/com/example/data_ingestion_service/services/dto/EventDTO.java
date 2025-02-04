cb9b317bd288b2d05bb278922cb950c63d8a3bd9
Timothy <timothysuldayan1@gmail.com>
2025-02-03 20:36:38 -0800

    fixed event dto issue by adding no args and all args constructors. ingtegration test for orchestrator fully works

--- data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/dto/EventDTO.java
+++ data-ingestion-service/src/main/java/com/example/data_ingestion_service/services/dto/EventDTO.java
@@ -3,14 +3,12 @@ package com.example.data_ingestion_service.services.dto;
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class EventDTO {
     @Nonnull
