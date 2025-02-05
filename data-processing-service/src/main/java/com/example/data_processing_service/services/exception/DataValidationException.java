090cd6cf21c5ee402978dda08c007163b1738f69
Timothy <timothysuldayan1@gmail.com>
2025-02-04 20:21:00 -0800

    configured custom data validation exception

---
+++ data-processing-service/src/main/java/com/example/data_processing_service/services/exception/DataValidationException.java
@@ -0,0 +1,7 @@
+package com.example.data_processing_service.services.exception;
+
+public class DataValidationException extends Exception {
+    public DataValidationException(String msg) {
+        super(msg);
+    }
+}
