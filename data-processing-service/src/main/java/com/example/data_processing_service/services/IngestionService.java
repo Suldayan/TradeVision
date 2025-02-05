1b18fda6ce5839cfed7bbcbf7eb52f886123fa70
Timothy <timothysuldayan1@gmail.com>
2025-02-04 20:07:29 -0800

    configured ingestion service interface

---
+++ data-processing-service/src/main/java/com/example/data_processing_service/services/IngestionService.java
@@ -0,0 +1,9 @@
+package com.example.data_processing_service.services;
+
+import com.example.data_processing_service.models.RawMarketModel;
+
+import java.util.Set;
+
+public interface IngestionService {
+    Set<RawMarketModel> fetchRawMarkets(Long timestamp);
+}
