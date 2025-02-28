2aa27bcea1ac2ba43de88129f0f9a2297ddf377b
Timothy <timothysuldayan1@gmail.com>
2025-02-28 12:40:00 -0800

    changed nested module name to Market DTO

--- trade-vision-backend/src/main/java/com/example/trade_vision_backend/ingestion/market/domain/dto/package-info.java
+++ trade-vision-backend/src/main/java/com/example/trade_vision_backend/ingestion/market/domain/dto/package-info.java
@@ -1,5 +1,5 @@
 @ApplicationModule(
-        displayName = "marketsDTO",
+        displayName = "Market DTO",
         allowedDependencies = "ingestion"
 )
 package com.example.trade_vision_backend.ingestion.market.domain.dto;
